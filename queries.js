f7cf6a44d2d0bfc9501b6c8d1dcfa27948b1b1bc
SaiphTG <zero-hour2010@hotmail.com>
2023-05-11 16:55:46 -0500

    queries updated

--- queries.js
+++ queries.js
@@ -6,3 +6,216 @@ const pool = new Pool({
   password: 'postgres',
   port: 5432,
 });
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const createUser = (request, response) => {
+  const {id, first_name, last_name, phone, address } = request.body[0];
+  
+  pool.query('INSERT INTO users (id, first_name, last_name, phone, address) VALUES ($1, $2, $3, $4, $5)', [id, first_name, last_name, phone, address], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${id}`)
+  })
+};
+
+const updateUser = (request, response) => {
+  const {id, first_name, last_name, phone, address } = request.body[0];
+  
+  pool.query(
+    'UPDATE users SET first_name = $2, last_name = $3, phone = $4, address = $5 WHERE id = $1',
+    [id,first_name, last_name, phone, address],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+};
+
+const getEnvelopes = (request, response) => {
+  pool.query('SELECT * FROM envelope ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const getEnvelopeById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM envelope WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const createEnvelope = (request, response) => {
+  const {id, first_name, last_name, phone, address } = request.body[0];
+
+  pool.query('INSERT INTO envelope (id, first_name, last_name, phone, address) VALUES ($1, $2, $3, $4, $5)', [id, first_name, last_name, phone, address], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+};
+
+const updateEnvelope = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { first_name, last_name, phone, address } = request.body
+
+  pool.query(
+    'UPDATE envelope SET name = $1, email = $2, phone = $3, address = $4 WHERE id = $5',
+    [first_name, last_name, phone, address, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+};
+
+const deleteEnvelope = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM envelope WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+};
+
+const getTransactions = (request, response) => {
+  pool.query('SELECT * FROM transactions ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const getTransactionById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM transactions WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const createTransaction = (request, response) => {
+  const {id, first_name, last_name, phone, address } = request.body[0];
+
+  pool.query('INSERT INTO transactions (first_name, last_name, phone, address) VALUES ($1, $2, $3, $4)', [first_name, last_name, phone, address], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+};
+
+const updateTransaction = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { first_name, last_name, phone, address } = request.body
+
+  pool.query(
+    'UPDATE transactions SET name = $1, email = $2, phone = $3, address = $4 WHERE id = $5',
+    [first_name, last_name, phone, address, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+};
+
+const deleteTransaction = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM transactions WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+};
+
+const getUserTransactions = (request, response) => {
+  pool.query('SELECT * FROM user_transaction ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const getUserTransactionById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM user_transactions WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  getEnvelopes,
+  getEnvelopeById,
+  createEnvelope,
+  updateEnvelope,
+  deleteEnvelope,
+  getTransactions,
+  getTransactionById,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+  getUserTransactions,
+  getUserTransactionById
+};
--- server.js
+++ server.js
@@ -3,11 +3,41 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const api = require('./api');
+const db = require('./queries')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use('/',api);
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+app.get('/envelope', db.getEnvelopes);
+app.get('/envelope/:id', db.getEnvelopeById);
+app.post('/envelope', db.createEnvelope);
+app.put('/envelope/:id', db.updateEnvelope);
+app.delete('/envelope/:id', db.deleteEnvelope);
+app.get('/transactions', db.getTransactions);
+app.get('/transactions/:id', db.getTransactionById);
+app.post('/transactions', db.createTransaction);
+app.put('/transactions/:id', db.updateTransaction);
+app.delete('/transactions/:id', db.deleteTransaction);
+app.get('/user_transaction', db.getUserTransactions);
+app.get('/user_transactions/:id', db.getUserTransactionById);
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening on ${PORT}`);
