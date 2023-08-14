09513ff70d6ed59745e401e5414538b05c4f347d
SaiphTG <zero-hour2010@hotmail.com>
2023-08-14 13:49:59 -0500

    Final commit for Personal Budget

--- api.js
+++ api.js
@@ -31,58 +31,5 @@ class envelope {
         return this._leftoverBudget=num;
     }  
 };
-/*
-const envTest = new envelope('food',1000,50);
 
-const envTests = new envelope('water',1000,50);
-
-const envTesting = new envelope('gas',1000);
-
-envelopes.push(envTest,envTests,envTesting);
-
-
-envelopeRouter = express.Router();
-apiRouter.use('/envelopes',envelopeRouter);
-
-envelopeRouter.param('envelopeId',(req,res,next,id)=>{
-    const index = req.body[0].id;
-    res.locals.findEnvelopeFromId = envelopes.filter(envelope => envelope.id===index)[0];
-    console.log(res.locals.findEnvelopeFromId);
-    if(res.locals.findEnvelopeFromId!==undefined){
-        next();
-    }else{
-        res.status(404).send();
-    }
-})
-
-envelopeRouter.get('/',(req,res,next)=>{
-    res.send(envelopes);
-});
-
-envelopeRouter.post('/',(req,res,next)=>{
-    let category = req.body.category;
-    let budget = req.body.budget;
-    if(typeof category!=='string'||isNaN(budget)===true){
-        res.status(400).send();
-    }else{
-        const newEnvelope = new envelope( category, budget );
-        envelopes.push(newEnvelope);
-        res.status(201).send(newEnvelope);
-    }
-});
-
-envelopeRouter.get('/:envelopeId',(req,res,next)=>{
-    res.send(res.locals.findEnvelopeFromId);
-});
-
-envelopeRouter.put('/:envelopeId',(req,res,next)=>{
-    res.locals.findEnvelopeFromId = req.body[0];
-    res.status(200).send(res.locals.findEnvelopeFromId);
-});
-
-envelopeRouter.delete('/:envelopeId',(req,res,next)=>{
-    envelopes.splice(res.locals.findEnvelopeFromId,1);
-    res.status(204).send(envelopes);
-});
-*/
 module.exports = apiRouter;
